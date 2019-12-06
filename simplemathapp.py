10e335532a01fff2f1924da786ff0e4729b13b46
webizfabulous <webizmoxx@gmail.com>
2019-12-06 09:10:41 +0545

    Made the code more logical

--- simplemathapp.py
+++ simplemathapp.py
@@ -6,30 +6,51 @@ class Math:
     #initializing a argument
     def __init__(self,argument):
         self.argument = argument
+        self.first_number = 0
+        self.last_number = 0
+        self.datas = [
+            'addition',
+            'add',
+            'subtract',
+            'sub',
+            'subtraction',
+            'multiply',
+            'multiplication',
+            'divide',
+            'division',
+        ]
 
-    #do_math function for calculation
-    def do_math(self,first_number,last_number):
-        if self.argument.lower() == 'addition' or self.argument.lower() == 'add':
-            return f"The addition value is {first_number+last_number}"
-        elif self.argument.lower() == 'subtract' or self.argument.lower() == 'sub' or self.argument.lower() == 'subtraction':
-            return f"The subtraction value is {first_number-last_number}"
-        elif self.argument.lower() == 'multiply' or self.argument.lower() == 'multiplication':
-            return f"The multiplication value is {first_number*last_number}"
-        elif self.argument.lower() == 'divide' or self.argument.lower() == 'division':
-            return f"The division value is {first_number/last_number}"
+    def run_function(self):
+        if self.argument in self.datas:
+            #first number as user input
+            self.first_number = int(input("Type first number "))
+            #second_number as user input
+            self.last_number = int(input("Type second number  "))
+            self.do_math()
         else:
             return "Sorry , command is out of reach"
 
+    #do_math function for calculation
+    def do_math(self):
+        if self.argument.lower() == 'addition' or self.argument.lower() == 'add':
+            return f"The addition value is {self.first_number+self.last_number}"
+        elif self.argument.lower() == 'subtract' or self.argument.lower() == 'sub' or self.argument.lower() == 'subtraction':
+            return f"The subtraction value is {self.first_number-self.last_number}"
+        elif self.argument.lower() == 'multiply' or self.argument.lower() == 'multiplication':
+            return f"The multiplication value is {self.first_number*self.last_number}"
+        elif self.argument.lower() == 'divide' or self.argument.lower() == 'division':
+            return f"The division value is {self.first_number/self.last_number}"
+
+
+
+
 #taking argument as use input 
 argument = input("What you want to do? ")
-#first number as user input
-first_number = int(input("Type first number "))
-#second_number as user input
-second_number = int(input("Type second number  "))
 
-#creating a object
+
+#creating a object and passing arugment
 math = Math(argument)
 
 #printing out the result
-print(math.do_math(first_number , second_number))
+print(math.run_function())
 
