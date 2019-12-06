50d9c5d6f7decbd2fb6870baeca0c064723ee05c
webizfabulous <webizmoxx@gmail.com>
2019-12-06 08:51:01 +0545

    Made the output looks much nicer

--- simplemathapp.py
+++ simplemathapp.py
@@ -10,22 +10,22 @@ class Math:
     #do_math function for calculation
     def do_math(self,first_number,last_number):
         if self.argument.lower() == 'addition' or self.argument.lower() == 'add':
-            return first_number+last_number
-        elif self.argument.lower() == 'subtract' or self.argument.lower() == 'sub':
-            return first_number - last_number
-        elif self.argument.lower() == 'multiply':
-            return first_number * last_number
-        elif self.argument.lower() == 'divide':
-            return first_number / last_number
+            return f"The addition value is {first_number+last_number}"
+        elif self.argument.lower() == 'subtract' or self.argument.lower() == 'sub' or self.argument.lower() == 'subtraction':
+            return f"The subtraction value is {first_number-last_number}"
+        elif self.argument.lower() == 'multiply' or self.argument.lower() == 'multiplication':
+            return f"The multiplication value is {first_number*last_number}"
+        elif self.argument.lower() == 'divide' or self.argument.lower() == 'division':
+            return f"The division value is {first_number/last_number}"
         else:
             return "Sorry , command is out of reach"
 
 #taking argument as use input 
-argument = input("What you want to do?/n")
+argument = input("What you want to do? ")
 #first number as user input
-first_number = int(input("Type first number/n"))
+first_number = int(input("Type first number "))
 #second_number as user input
-second_number = int(input("Type second number/n"))
+second_number = int(input("Type second number  "))
 
 #creating a object
 math = Math(argument)
